4e24142ab484abafb2474893c0de6e5ba8ebef13
Taras <vtaras15@gmail.com>
2020-02-22 12:05:05 +0200

    now blocks cannot climb on each other

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ Http.listen(3000, () => {
   console.log("port 3000")
 });
 
-var position = {
-  x: 400,
-  y: 200
-};
+var point = 5
+
+var board = [
+  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+  [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+  [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0],
+  [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0],
+  [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+  [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+  [ 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
+  [-1, 0, 1, 0, 1, 1, 0, 0, 0, 0]
+];
 
 var players = {}
 
@@ -20,22 +30,37 @@ Socketio.on("connection", socket => {
       x: 400,
       y: 200
     }
+    Socketio.emit('maze', point)
+  })
+
+  
+
+  socket.on("disconnect", () => {
+    delete (players[socket.id])
   })
 
   socket.on("move", data => {
     var player = players[socket.id] || {}
     switch (data) {
       case "left":
-        player.x -= 5;
+        if (!findElementByX(player.x - 20, player.y, socket.id)) {
+          player.x -= 5;
+        } else player.x += 10;
         break;
       case "right":
-        player.x += 5;
+        if (!findElementByX(player.x + 20, player.y, socket.id)) {
+          player.x += 5;
+        } else player.x -= 10;
         break;
       case "up":
-        player.y -= 5;
+        if (!findElementByY(player.y - 20, player.x, socket.id)) {
+          player.y -= 5;
+        } else player.y += 10;
         break;
       case "down":
-        player.y += 5;
+        if (!findElementByY(player.y + 20, player.x, socket.id)) {
+          player.y += 5;
+        } else player.y -= 10;
         // position.y = position.y + 5;
         // Socketio.emit("position", { pos: players[socket.id], direction: "down" })
         break;
@@ -46,3 +71,29 @@ Socketio.on("connection", socket => {
 setInterval(() => {
   Socketio.emit('state', players)
 }, 1000 / 60)
+
+
+
+function findElementByX(coordinateX, coordinateY, id) {
+  for (var num in players) {
+    var player = players[num];
+    if (player.id != id && player.x == coordinateX) {
+      if ((player.y >= coordinateY && player.y < coordinateY + 20) || (player.y <= coordinateY && player.y > coordinateY - 20)) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+function findElementByY(coordinateY, coordinateX, id) {
+  for (var num in players) {
+    var player = players[num];
+    if (player.id != id && player.y == coordinateY) {
+      if ((player.x >= coordinateX && player.x < coordinateX + 20) || (player.x <= coordinateX && player.x > coordinateX - 20)) {
+        return true
+      }
+    }
+  }
+  return false
+}
